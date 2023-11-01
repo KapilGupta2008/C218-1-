feccd4fe0afa57bcba06773e36682933c7d83efa
Kapil <83456050+KapilGupta2008@users.noreply.github.com>
2023-11-01 15:38:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,39 @@
-const expess=require("express")
-const app =expess()
-const server=require("http").Server(app)
-const {v4:uuidv4}=require("uuid")
-const io=require("socket.io")(server,{
-    cors:{origin:"*"}
-})
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+const { v4: uuidv4 } = require("uuid");
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
     debug: true,
 });
 
-app.use("/peerjs",peerServer)
-app.set("view engine","ejs")
-app.use(expess.static("public"))
+app.use("/peerjs", peerServer);
 
+app.get("/", (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+});
 
-app.get("/",(req,res)=>{
-    res.redirect(`/${uuidv4()}`)
-})
-app.get("/:room",(req,res)=>{
-    res.render("index",{roomId:req.params.room})
-})
-io.on("connection",(socket)=>{
-    socket.on("join-room",(roomId,userId,user)=>{
-        socket.join(roomId)
-        socket.on("message",(message)=>{
-            io.to(roomId).emit("create_message",message,username)
+app.get("/:room", (req, res) => {
+    res.render("index", { roomId: req.params.room });
+});
 
-        })
-        
-        
-    })
-    
-})
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId, userName) => {
+        socket.join(roomId);
+        socket.on("message", (message) => {
+            io.to(roomId).emit("createMessage", message, userName);
+        });
+    });
+});
 
-server.listen(3030)
-
+server.listen(process.env.PORT || 3030);
